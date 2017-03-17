6f94af9135fa8f42ff66944e975db0fa55e696fb
Tanel Prikk <tanel.prikk@outlook.com>
2017-03-17 13:45:39 +0200

    no message

---
+++ src/main/java/ee/ttu/idk0071/sentiment/model/Business.java
@@ -0,0 +1,34 @@
+package ee.ttu.idk0071.sentiment.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Business {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+
+	public Business() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/ee/ttu/idk0071/sentiment/repository/SentimentTypeRepository.java
@@ -0,0 +1,7 @@
+package ee.ttu.idk0071.sentiment.repository;
+
+import ee.ttu.idk0071.sentiment.model.SentimentType;
+
+public interface SentimentTypeRepository extends ReadOnlyRepository<SentimentType, String>{
+
+}
