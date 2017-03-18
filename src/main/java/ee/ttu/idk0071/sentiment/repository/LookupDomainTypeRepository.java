35055959eb0671837e211a828e2e76e13f600613
Tanel Prikk <tanel.prikk@outlook.com>
2017-03-18 20:11:59 +0200

    forgot to add repository

---
+++ src/main/java/ee/ttu/idk0071/sentiment/repository/LookupDomainTypeRepository.java
@@ -0,0 +1,7 @@
+package ee.ttu.idk0071.sentiment.repository;
+
+import ee.ttu.idk0071.sentiment.model.LookupDomainType;
+
+public interface LookupDomainTypeRepository extends ReadOnlyRepository<LookupDomainType, Integer> {
+
+}
