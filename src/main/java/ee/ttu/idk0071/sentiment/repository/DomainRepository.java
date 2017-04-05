4c255f2d5362483d73316cfe8f8752174a79365c
Margus Tanneberg <m.tanneberg@gmail.com>
2017-04-05 18:52:57 +0300

    Domain Repo done

--- src/main/java/ee/ttu/idk0071/sentiment/repository/DomainRepository.java
+++ src/main/java/ee/ttu/idk0071/sentiment/repository/DomainRepository.java
@@ -1,7 +1,10 @@
 package ee.ttu.idk0071.sentiment.repository;
 
+import java.util.List;
+
 import ee.ttu.idk0071.sentiment.model.Domain;
 
 public interface DomainRepository extends ReadOnlyRepository<Domain, Integer> {
 	public Domain findByName(String name);
+	List<Domain> findByActiveTrue();
 }
